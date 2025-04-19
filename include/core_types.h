216d618d1c29a23319a39a763c38541c6692daa8
dan <daniel.guy.moody@gmail.com>
2025-04-19 02:11:51 +0100

    whitespace

--- include/core_types.h
+++ include/core_types.h
@@ -58,10 +58,10 @@ SOFTWARE.
 #define RFor( T, it, start, count )	for ( T it = (count); it-- > (start); )
 
 // returns the amount of padding required to align x up to the next aligned address
-#define padding_up( x, alignment )	( (alignment) - 1 )  & ~( (alignment) - 1 )
+#define padding_up( x, alignment )	( (alignment) - 1 ) & ~( (alignment) - 1 )
 
 // returns the input 'x' that has been aligned up by 'alignment' to the next largest value, in bytes
-#define align_up( x, alignment )	( (x) +  padding_up( x , alignment ) )
+#define align_up( x, alignment )	( (x) + padding_up( x, alignment ) )
 
 typedef int8_t		s8;
 typedef int16_t		s16;
